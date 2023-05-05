5d1b0441c6ed6eca50fb84a4a2a495325263a2c2
Joseph Jiménez <=>
2023-05-04 22:58:47 -0400

    Agregando typewriter en el input

---
+++ assets/js/animations.js
@@ -0,0 +1,70 @@
+(function($) {
+
+
+// List of sentences
+var _CONTENT = [ 
+	"Buscar 🔍", 
+	"UI/UX Diseño Web", 
+	"Community Manager", 
+	"Diseño de Logos"
+];
+
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed 
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Implements typing effect
+function Type() { 
+	// Get substring with 1 characater added
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+   $('#searchInput').attr('placeholder', text + "|")
+	//_ELEMENT += text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === _CONTENT[_PART]) {
+      $('#searchInput').attr('placeholder', text)
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 200);
+		}, 1000);
+	}
+}
+
+// Implements deleting effect
+function Delete() {
+	// Get substring with 1 characater deleted
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+   $('#searchInput').attr('placeholder', text)
+	//_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	// If sentence has been deleted then start to display the next sentence
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		// If current sentence was last then display the first one, else move to the next
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		
+		_PART_INDEX = 0;
+
+		// Start to display the next sentence after some time
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Type, 200);
+		}, 300);
+	}
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 200);
+
+})(jQuery);
+
